54367fa40f5b831fad3ce86aa8392dad67a1e148
Aleinys-Mendoza <aleinismendoza15@gmail.com>
2024-10-15 11:53:17 -0500

    Add files via upload

---
+++ app_empresa/app/config.py
@@ -0,0 +1,22 @@
+#Importando libreria mysql.connector para poder conectar Python con MySQL
+import mysql.connector
+
+def connectionBD(): 
+     try:
+       #connection = mysql.connector.connect(
+       connection = mysql.connector.connect(
+            host="localhost", 
+            user="root", 
+            passwd="", 
+            database="app_empresa_bd", 
+            charset='utf8mb4', 
+            collation='utf8mb4_unicode_ci', 
+            raise_on_warnings=True
+          ) 
+       
+       if connection.is_connected():
+           #print("Conexion exitosa a la BD")
+           return connection
+     
+     except mysql.connector.Error as error:
+         print(f"No se puede conectar:{error}")
